7119a0656647669ff9a2a36d7692173eed859376
Raphael Winkler <raphael84@live.at>
2018-11-24 12:34:29 +0100

    Embedded Material design dependencies into Installer

--- Installer/App.xaml.cs
+++ Installer/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Installer.Objects;
+using System;
+using System.IO;
+using System.Reflection;
 using System.Windows;
 
 namespace Installer
@@ -8,9 +11,26 @@ namespace Installer
     /// </summary>
     public partial class App : Application
     {
-        public void ChangeTheme(Uri uri)
+        [STAThread()]
+        //[DebuggerNonUserCode()]
+        public static void Main()
         {
-            Resources.MergedDictionaries[0] = new ResourceDictionary() { Source = uri };
+            App app = new App()
+            {
+                ShutdownMode = ShutdownMode.OnMainWindowClose
+            };
+            EmbeddedAssembly.Load("Installer.MaterialDesignColors.dll", "MaterialDesignColors.dll");
+            EmbeddedAssembly.Load("Installer.MaterialDesignThemes.Wpf.dll", "MaterialDesignThemes.Wpf.dll");
+
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+
+            app.InitializeComponent();
+            app.Run();
+        }
+
+        private static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            return EmbeddedAssembly.Get(args.Name);
         }
     }
 }
--- Uninstaller/App.xaml.cs
+++ Uninstaller/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using System.Windows;
 
 namespace Uninstaller
@@ -8,10 +9,26 @@ namespace Uninstaller
     /// </summary>
     public partial class App : Application
     {
-        public void ChangeTheme(Uri uri)
+        [STAThread()]
+        //[DebuggerNonUserCode()]
+        public static void Main()
         {
-            if (uri != null)
-                Resources.MergedDictionaries[0] = new ResourceDictionary() { Source = uri };
+            App app = new App()
+            {
+                ShutdownMode = ShutdownMode.OnMainWindowClose
+            };
+            EmbeddedAssembly.Load("Uninstaller.MaterialDesignColors.dll", "MaterialDesignColors.dll");
+            EmbeddedAssembly.Load("Uninstaller.MaterialDesignThemes.Wpf.dll", "MaterialDesignThemes.Wpf.dll");
+
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+
+            app.InitializeComponent();
+            app.Run();
+        }
+
+        private static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            return EmbeddedAssembly.Get(args.Name);
         }
     }
 }
